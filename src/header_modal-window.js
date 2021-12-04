7311cd1c7e9f29cce38945fcb28546bce54f19bb
marinapuhach <pugach.marina77@gmail.com>
2021-12-04 16:53:17 +0200

    false

---
+++ src/header_modal-window.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-header-modal-open]'),
+      closeModalBtn: document.querySelector('[data-header-modal-close]'),
+      modal: document.querySelector('[data-header-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      document.body.classList.toggle('modal-open')
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
