7c708686ec67f41b804c8381da790699bc6fbd1d
Anna-Babynina <annyajoy@ukr.net>
2021-12-05 23:00:49 +0200

    добавила третью модалку

--- src/product-modal.js
+++ src/product-modal.js
@@ -29,4 +29,21 @@
       refs.modal.classList.toggle('is-hidden-beige');
     }
 })();
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-product-green-modal-open]'),
+      closeModalBtn: document.querySelector('[data-product-green-modal-close]'),
+      modal: document.querySelector('[data-product-green-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+    //   document.body.classList.toggle('modal-open')
+      refs.modal.classList.toggle('is-hidden-green');
+    }
+})();
+
   
