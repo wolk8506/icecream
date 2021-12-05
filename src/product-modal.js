47d98623212a57b6e4ae4118505e47d579427ebd
Anna-Babynina <annyajoy@ukr.net>
2021-12-05 11:48:19 +0200

    Модалка для products

---
+++ src/product-modal.js
@@ -0,0 +1,16 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-product-modal-open]'),
+      closeModalBtn: document.querySelector('[data-product-modal-close]'),
+      modal: document.querySelector('[data-product-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+    //   document.body.classList.toggle('modal-open')
+      refs.modal.classList.toggle('is-hidden');
+    }
+})();
+  
