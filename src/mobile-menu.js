3bce3cd82c5e2bf7d7e5e263ea03d17283d73aa9
Yuriy Krasukov <wolk8506@gmail.com>
2021-12-06 22:01:01 +0200

    закрытие мобильного меню при переходе по секциям

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,6 +2,11 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuHome = document.querySelector('.js-close-home');
+  const closeMenuAbout = document.querySelector('.js-close-about');
+  const closeMenuHowitsmade = document.querySelector('.js-close-howitsmade');
+  const closeMenuProduvts = document.querySelector('.js-close-produvts');
+  const closeMenuContact = document.querySelector('.js-close-contact');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -14,6 +19,11 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuHome.addEventListener('click', toggleMenu);
+  closeMenuAbout.addEventListener('click', toggleMenu);
+  closeMenuHowitsmade.addEventListener('click', toggleMenu);
+  closeMenuProduvts.addEventListener('click', toggleMenu);
+  closeMenuContact.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
